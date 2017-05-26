87fb01aced10978d2983a2ae88e1c0898851823b
Kiên Nguyễn <nguyenphuctrungkien@gmail.com>
2017-05-26 22:25:49 +0700

    Added AnimatedLinearGradient for Profile User

--- src/screens/authorized/profile/Profile.js
+++ src/screens/authorized/profile/Profile.js
@@ -1,25 +1,28 @@
 import React, { Component } from 'react';
-import { View, Text, Image,Button } from 'react-native';
+import { View, Text, Image, StyleSheet } from 'react-native';
+import AnimatedLinearGradient , { presetColors } from 'react-native-animated-linear-gradient';
 import { Header_Home } from '../../../components/headers/Header_Home';
 import Profile_View from './Profile_View';
 
+
 class Profile extends Component {
-     static navigationOptions = {
-        
+    static navigationOptions = {
+
         title: 'Home',
         headerTitleStyle: { color: '#fff' },
-        header:(navigation) => <Header_Home {...navigation}/>
+        header: (navigation) => <Header_Home {...navigation} />
     }
     render() {
         return (
-            <View style={{flex:1}}>
-                <Text>AAAAA</Text>
-             <Profile_View goto={() => {this.props.navigation.navigate('Change_Info')}}/>
-             </View>
+
+            <AnimatedLinearGradient customColors={presetColors.instagram} speed={4000} />
+
         );
     }
-    
-};
-
+}
 
 export default Profile;
+
+/**
+ *  <Profile_View goto={() => { this.props.navigation.navigate('Change_Info') }} />
+ */
